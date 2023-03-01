d230fc89213d2a416df5eed96f24ffe5e25d049e
Chirag Arora <chirag1144@chitkara.edu.in>
2023-03-02 01:02:09 +0530

    Intial commit

--- app.js
+++ app.js
@@ -2,37 +2,26 @@ const express=require('express');
 const bodyParser=require('body-parser');
 const app=express();
 app.set("view engine","ejs");
-app.get('/',function(req,res){
+app.use(bodyParser.urlencoded({extended:true}));
+var val="";
+var arr=[];
+app.get('/',(req,res)=>{
   var today=new Date();
-  var currDay=today.getDay();
-  var day="";
- switch(currDay){
-    case 0:
-        day="Sunday";
-        break;
-    case 1:
-        day="Monday";
-        break;
-    case 2:
-        day="Tuesday";
-        break;
-    case 3:
-        day="Wednesday";
-        break;
-    case 4:
-        day="Thursday";
-        break;
-    case 5:
-        day="Friday";
-        break;
-    case 6:
-        day="Saturday";
-        break;
-    default:
-        console.log("Error");
- }
-  res.render("list",{kindOfDay:day});
-}) 
+  var options={
+    weekday:"long",
+    day:"numeric",
+    month:"long"
+  }; 
+  var day=today.toLocaleDateString("en-us",options);
+  res.render("list",{kindOfDay:day,newListItems:arr});
+})
+
+app.post('/',(req,res)=>{
+  val=req.body.newItem;
+  arr.push(val);
+  res.redirect('/');
+
+})
 app.listen(3000,()=>{
-    console.log("Server is running at port 3000");
+  console.log("Server is running at port 3000");
 })
